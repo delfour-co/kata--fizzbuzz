2da1ea8f9765d8b04fdd1a606898f60c9519ae6f
Kevin Delfour <kevin.delfour@jobo-ci.com>
2025-11-19 08:58:39 +0100

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function fizzbuzz(num) {
+	// Implement code here    
+	return num
+}
+
+
+for (let i = 1; i <= 100; i++) {
+	console.log(fizzbuzz(i))
+}
+
+module.exports = fizzbuzz;
---
+++ index.test.js
@@ -0,0 +1,5 @@
+const fizzbuzz = require('./index');
+
+test('fizzbuzz returns the correct output for a given input', () => {
+    // Implement tests here
+});
